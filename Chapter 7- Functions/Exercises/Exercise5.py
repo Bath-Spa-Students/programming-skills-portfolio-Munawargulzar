1bcdcba9a36e9db0925f4e3b08d9fc75f120d7ed
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-04-03 15:50:06 +0400

    Create Exercise5.py
    excercise 5

---
+++ Chapter 7- Functions/Exercises/Exercise5.py
@@ -0,0 +1,7 @@
+def decribe_city(city, country='Iceland'):
+    """ Describe a city and it's country."""
+    print(f"{city.title()}) is in {country}.")
+
+    decribe_city ('reyljavik')
+    decribe_city ('london', 'spain')
+    decribe_city ('sialkot,' , 'italy')
