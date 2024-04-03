4a8123255d0d9c1daa27a0a0ced1fbcd8ccb596a
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-04-03 15:44:30 +0400

    Create Exercise3.py
    excercise 3

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,5 @@
+favorite_tarnsport = ["aeroplane", "bike", "car"]
+
+print("I love travelling  " + favorite_tarnsport [0] + "to united kingdom.")
+print("I like to ride" + favorite_tarnsport [1] + " on bike to avoid traffic.")
+print("It would be fun to ride in "  + favorite_tarnsport[2] + "my bmw in summers.")
