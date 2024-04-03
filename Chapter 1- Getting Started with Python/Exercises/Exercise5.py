af3258692664513c6d8e386799e21c55b0170370
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-04-03 15:41:34 +0400

    Create Exercise5.py
    excercise 5

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise5.py
@@ -0,0 +1,14 @@
+#python program to find the area of circle
+
+# take input from the user of the radius
+radius= float(input("Enter the radius of the circle"))
+
+# use the constant value of pie
+PI =3.14
+
+#calculate the area 
+area = PI *radius *radius
+
+#result
+
+print(f"Area of the circle={area:.2f}")
