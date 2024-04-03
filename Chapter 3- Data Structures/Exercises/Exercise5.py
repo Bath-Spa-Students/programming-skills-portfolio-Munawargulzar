a8ae306c4914745500a1da7ef2eb176db24520f2
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-04-03 15:45:01 +0400

    Create Exercise5.py
    excercise 5

---
+++ Chapter 3- Data Structures/Exercises/Exercise5.py
@@ -0,0 +1,20 @@
+guests = ["huzaifa", "amber","huzamber"]
+print("Dear" + guests[0]+"  , please come to my dinner prty,")
+print("Dear" + guests[1]+"  , please come to my dinner prty,")
+print("Dear" + guests[2]+"  , please come to my dinner prty,")
+
+print("\nSorry, " + guests[2] + "can no longer attend")
+
+guests[2] = "huzaima"
+
+print("\nNew gusest list:")
+print(guests)
+
+print("\nSending new invitations...")
+print("Dear" + guests[0]+"  , please come to my dinner prty,")
+print("Dear" + guests[1]+"  , please come to my dinner prty,")
+print("Dear" + guests[2]+"  , please come to my dinner prty,")
+
+
+
+
