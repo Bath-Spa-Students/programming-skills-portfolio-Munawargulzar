474bb76373a28c3fb95748677627527d5d148cb1
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-04-03 15:45:40 +0400

    Create Exercise7.py
    excercise 7

---
+++ Chapter 3- Data Structures/Exercises/Exercise7.py
@@ -0,0 +1,28 @@
+locations = ['united kingdom', 'united states,' , 'italy', 'pakistan', 'turkey']
+
+print("original order:")
+print(locations)
+
+print("\nAlphabetical:")
+print(locations)
+
+print("\nOriginal order:")
+print(locations)
+
+print("\nReversed:")
+locations.reverse()
+print(locations)
+
+print("\nReversed:")
+locations.reverse()
+print(locations)
+
+print("\nAlphabetical:")
+locations.sort()
+print(locations)
+
+print("\nReverse alphabetical:")
+locations.sort(reverse=True)
+print(locations)
+
+
