4b8f5a999ebab4e8f91a8a2c31608c55fe775a10
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-04-03 15:46:29 +0400

    Create Exercise5.py
    excercise 5

---
+++ Chapter 4- Control Flow/Exercises/Exercise5.py
@@ -0,0 +1,22 @@
+favourite_fruits = {'mango','strawberry','cherry'}
+
+if 'mango' in favourite_fruits: 
+    print ('you really like mango')
+
+if 'strawberry' in favourite_fruits:
+   print ('you really like strawberry')
+
+if 'cherry' in favourite_fruits:  
+    print ('you really like cherry')
+           
+if 'apple' in favourite_fruits:
+     print ('you really like apple')
+
+        
+
+          
+
+
+
+
+
