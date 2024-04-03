af9e3f28c074dfe84e95430d3afd741049b22dc2
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-04-03 15:45:48 +0400

    Create Exercise1.py
    excercise 1

---
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -0,0 +1,14 @@
+#PASSING VERSION 
+
+alien_color = "greem"
+
+if alien_color == "green":
+    print("you just earned 5 points!")
+
+
+    #FALLING VERSION
+
+    alien_color = "yellow"
+    
+    if alien_color == "green":
+        print("you just earned 5")
