329c900b1a30dda6e714c3015bb42070f127eef4
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-04-03 15:47:07 +0400

    Create Exercise2.py
    excercise 2

---
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -0,0 +1,23 @@
+glossary ={ 
+    'string': 'A series of characters.',
+    'comment': 'A note in a program thatb the python interoreter ignores.',
+    'list': 'A collection of itenms in a particular order.',
+    'loop': 'work through a collection of items, one at a time.',
+    'dictionary': "A collection of key-value pairs.",
+}
+
+word = 'string'
+print("\n" + word.title()+": " + glossary[word])
+
+word = 'comment'
+print("\n" + word.title()+": " + glossary[word])
+
+words = 'list'
+print("\n" + word.title()+": " + glossary[word])
+
+word = 'loop'
+print("\n" + word.title()+": " + glossary[word])
+
+word = 'dictionary'
+print("\n" + word.title()+": " + glossary[word])
+
