5b745f0be8cceef368f3641479f8e1ca34fb316b
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-04-03 15:47:48 +0400

    Create Exercise4.py
    excercise 4

---
+++ Chapter 5- Dictionaries/Exercises/Exercise4.py
@@ -0,0 +1,18 @@
+rivers = {
+    'nile': 'egypt',
+    'mississippi' : 'united states',
+    'fraser': 'canada'
+}
+
+for rivers , country in rivers.items():
+    print(f"The {rivers.title()} runs throught {country.title()}.")
+
+print("\nThe rivers are:")
+for rivers in rivers.keys():
+    print (rivers.tittle())
+    
+    print("\nThe countries are:")
+    for rivers in rivers.keys():
+     print (rivers.tittle())
+
+
