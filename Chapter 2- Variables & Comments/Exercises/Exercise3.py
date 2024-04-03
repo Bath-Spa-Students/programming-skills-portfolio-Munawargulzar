20b9fa4589952fe8d661c617a2ffe1fa406fe43a
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-04-03 15:43:03 +0400

    Create Exercise3.py
    excercise 3

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -0,0 +1,16 @@
+# Define the person's name with whitespace characters
+person_name = "\n\t huzamber \t\n"
+
+#print the name whitespace around it
+print("name with whitespace around it:")
+print(person_name)
+
+#print the name using stripping function
+print("\nName using 1strip():")
+print(person_name.Istrip())
+
+print("\nName using rstrio():")
+print(person_name.rstrip())
+
+print("\nName using streip():")
+print(person_name.strip())
