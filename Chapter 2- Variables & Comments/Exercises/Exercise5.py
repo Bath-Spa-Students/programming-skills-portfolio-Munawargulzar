6dc0b4abc4c3e130c8b2d4838e692845853e8095
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-04-03 15:43:27 +0400

    Create Exercise5.py
    excercise 5

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise5.py
@@ -0,0 +1,6 @@
+budget = 210
+USB_cost = 900
+num_USB = budget / USB_cost
+remainder = budget - (num_USB * USB_cost)
+print( "numbers of the usb  that can be bought;",num_USB)
+print("money left over;" , remainder)
