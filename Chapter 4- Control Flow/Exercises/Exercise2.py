ce8c149540745eb43b7315a9e80ff3b80cb1efa2
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-04-03 15:45:59 +0400

    Create Exercise2.py
    excercise 2

---
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -0,0 +1,19 @@
+#IF BLOCK VERSION 
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("you just earned 5 points!")
+else:
+     print("you just earned 10 points!")
+
+
+     # ELSE BLOCK VERSION 
+
+     alien_color = 'yellow'
+
+     if alien_color == 'green':
+          print("you just earned 5 points!")
+     else:
+          print("you just earned 10 points!")
+          
