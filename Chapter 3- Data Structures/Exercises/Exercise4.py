290c162bf64d3a716d13de9ae0edd19073923e10
Munawargulzar <163411628+Munawargulzar@users.noreply.github.com>
2024-04-03 15:44:39 +0400

    Create Exercise4.py
    excercise 4

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,20 @@
+guests = ["huzaifa", "amber", "huzamber"]
+
+print("Dear" + guests[0] + ",")
+print("I am hosting a dinner party and hopen you can attend.")
+
+print("Dear" + guests[1] + ",")
+print("I am hosting a dinner party and hopen you can attend.")
+
+print("Dear" + guests[2] + ",")
+print("I am hosting a dinner party and hopen you can attend.")
+
+
+
+
+
+
+
+
+
+
